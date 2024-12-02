96a6e7f6d142ac7099f9302a7924d8171a7a62de
Vazha20 <gabisoniavazha@gmail.com>
2024-12-02 08:24:45 +0400

    bbbb

--- public/script.js
+++ public/script.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArPD1cOhp4v0ZfIyJ20yo6CWYgvYY9J-g",
+  authDomain: "georest00.firebaseapp.com",
+  projectId: "georest00",
+  storageBucket: "georest00.firebasestorage.app",
+  messagingSenderId: "645050361493",
+  appId: "1:645050361493:web:4b10277fad4e0cc88a5d9c",
+  measurementId: "G-498ST8ETY6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 let slider = document.querySelector('.slider .list');
 let items = document.querySelectorAll('.slider .list .item');
 let next = document.getElementById('next');
